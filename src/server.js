b54e67a90444122300e7d9f0ca5a25fc1d989e54
Hamilton Ribeiro <htadeu@hotmail.com>
2020-08-08 03:12:21 -0300

    Refactoring project-nlw2
    - Template Engine
    - Instalar e configurar `nunjucks`
    - Renderizar
    - Enviar dados para o HTML

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+// Dados
 const proffys = [
     {
         name:"Diego Fernandes",
         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp:"+55 11 91234-5678",
-        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        whatsapp:"11 91234-5678",
+        bio:"Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject:"Química",
         cost:"20",
         weekday:[0],
@@ -20,24 +21,69 @@ const proffys = [
         weekday:[1],
         time_for:[720],
         time_to:[1220]
+    },
+    {
+        name:"Mayk Brito",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp:"+55 11 91234-5678",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"20",
+        weekday:[1],
+        time_for:[720],
+        time_to:[1220]
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+// Funcionalidades
 function pageLanding(req, res){
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy(req, res){
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
 
-function pageGiveClasses(req, res){
-    return res.sendFile(__dirname + "/views/give_classes.html")
+function pageGiveClasses(req, res){    
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
+// Servidor
 const express = require('express')
 const server = express()
 
+// configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+// Inicio e configuração do servidor
 server
 // configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
@@ -45,4 +91,5 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+// start do servidor
 .listen(5500)
